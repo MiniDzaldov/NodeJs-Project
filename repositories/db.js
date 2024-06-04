03dd9b1f6ec915921fc8f0ec66f9223095b1f64e
דיזלדוב מינה < @mbjcomp.local>
2024-06-04 16:31:40 +0300

    aa

--- repositories/db.js
+++ repositories/db.js
@@ -4,8 +4,8 @@ mongoose.set("strictQuery", false);
 // const uri = 'mongodb://priority:27017/Leah_Mini_DB';
 
 export default async function connect() {
-    // await mongoose.connect('mongodb://priority:27017/?Leah_Mini_DB')
-    await mongoose.connect('mongodb+srv://Mini:mini4151@cluster0.r9plqqb.mongodb.net/Leah_Mini')
+    await mongoose.connect('mongodb://priority:27017/Leah_Mini_DB').catch(console.log(new Error('error')));
+    // await mongoose.connect('mongodb+srv://Mini:mini4151@cluster0.r9plqqb.mongodb.net/Leah_Mini')
 
     // await mongoose.connect(uri)
 
